ec79901a60aa1ac185fd3e6303e298da97065b91
mauguzun <mauguzun@gmail.com>
2020-10-10 19:18:46 +0300

    init

---
+++ FolloutBoard/Application/Application/Models/Entities/Simple.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Models.Entities
+{
+    class Simple
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Descrpition { get; set; }
+        public string Img { get; set; }
+        public string Extra { get; set; }
+    }
+}
---
+++ FolloutBoard/OcrReader/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\repos\FolloutBoard\FolloutBoard\Assets\вещи, перки, влияние, герои\vlijanie\";
+            var directories = Directory.GetDirectories(path);
+
+            var cards = new List<Card>();
+            foreach (var dirc in directories)
+            {
+                foreach (var filename in Directory.GetFiles(dirc))
+                {
+                    var ocrtext = string.Empty;
+                    using (var engine = new TesseractEngine(@"D:\repos\FolloutBoard\Utils\OCr\tessdata-3.04.00\", "rus", EngineMode.TesseractAndLstm))
+                    {
+                        using (var img = PixConverter.ToPix(new Bitmap(filename)))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                ocrtext = page.GetText();
+                            }
+                        }
+                    }
+                    var lines = ocrtext.Split(new string[] { "\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+                    Card card = new Card();
+                    card.Class = "influence";
+                    card.FileName = filename;
+                    card.Score = new DirectoryInfo(filename).Parent.Name;
+                    card.Lines = lines;
+                    cards.Add(card);
+
+                }
+
+            }
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<Card>));
+
+            // Create the TextWriter for the serialiser to use
+            TextWriter filestream = new StreamWriter(path + "data.xml");
+
+            //write to the file
+            serialiser.Serialize(filestream, cards);
+
+            // Close the file
+            filestream.Close();
+
+        }
+    }
+}
